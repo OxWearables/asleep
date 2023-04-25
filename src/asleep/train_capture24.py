747c2e3580697c9221d022c5a22eb585d8c84115
angerhang <514140809@qq.com>
2023-04-25 22:08:27 +0100

    fixed training script for cap24

--- src/asleep/train_capture24.py
+++ src/asleep/train_capture24.py
@@ -1,8 +1,10 @@
 import os
 import numpy as np
 import pandas as pd
+import sys
+sys.path.append('..')
 
-from alseep.models import SleepWindowSSL
+from asleep.models import SleepWindowSSL
 from utils import resize
 import joblib
 
